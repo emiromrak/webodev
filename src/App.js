4694552fad7284e4ef5ae4bf01c66a9c8f126953
emiromrak <62642855+emiromrak@users.noreply.github.com>
2025-10-31 17:14:30 +0300

    Remove comments and clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// AppState, Context klasöründen default export ile çekiliyor
+
 import AppState from './context/AppState'; 
 import Home from './pages/Home';
 import ShowDetail from './pages/ShowDetail';
@@ -8,14 +8,13 @@ import './App.css';
 
 function App() {
   return (
-    // Uygulama genelinde useReducer state'ini AppState ile sarmalıyoruz
     <AppState>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Dizi detayları için dinamik rota */}
+          {}
           <Route path="/detail/:id" element={<ShowDetail />} />
-          {/* Tanımlanmayan tüm yollar için Home'a yönlendirme */}
+          {}
           <Route path="*" element={<Home />} /> 
         </Routes>
       </Router>
