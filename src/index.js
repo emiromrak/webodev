e62a6a94fee53717875278410541030cb4a2aeba
emiromrak <62642855+emiromrak@users.noreply.github.com>
2025-10-31 17:14:56 +0300

    Clean up index.js by removing unused imports
    Removed unused CSS import and commented-out code.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Varsayılan CSS'i (index.css) projemizde kullanmadığımız için kaldırıyoruz veya siliyoruz.
-// import './index.css'; 
 
-import App from './App'; // Yeni App.js'imizi import ediyoruz
-// import reportWebVitals from './reportWebVitals'; // Bu da gereksiz
+
+import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> {/* Yeni Router yapısı içeren App'i render ediyoruz */}
+    <App /> {}
   </React.StrictMode>
 );
-
-// Eğer raporlama kullanmıyorsanız bu kısmı silebilirsiniz
-// reportWebVitals();
